487a3ea2962583dd62163a1f6558786b376926b6
Muhikira <muhikiraismael@gmail.com>
2023-03-03 12:18:22 -0700

    A short project on CRUD API, During this project I created a list of employees , i can post, read, update and delete any names. adding swagger -ui

--- src/main/java/com/example/springbootbackend/SpringBootBackendApplication.java
+++ src/main/java/com/example/springbootbackend/SpringBootBackendApplication.java
@@ -1,9 +1,12 @@
 package com.example.springbootbackend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "List of API", version = "1.0", description = "All the APIs Information"))
 public class SpringBootBackendApplication {
 
     public static void main(String[] args) {
